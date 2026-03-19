63927d90c543aefc108cb833b94d63c6c2589105
SupriyaPE <supriyape2002@gmail.com>
2026-03-20 00:08:36 +0530

    Added Event Loop Examples

---
+++ Day-1/file6.js
@@ -0,0 +1,30 @@
+console.log("1");
+
+setTimeout(()=>{ 
+    console.log("2");
+},0);
+
+setTimeout(()=>{ 
+    console.log("3");
+
+    Promise.resolve().then(()=>{
+      console.log("4"); 
+   })
+
+},0);
+
+
+Promise.resolve().then(()=>{
+    console.log("5"); 
+
+    setTimeout(()=>{ 
+        console.log("6");
+    },0);
+})
+
+
+Promise.resolve().then(()=>{
+    console.log("7"); 
+})
+
+console.log("8");
